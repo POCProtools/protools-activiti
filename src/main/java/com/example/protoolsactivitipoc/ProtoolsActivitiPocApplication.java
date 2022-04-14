697035b0d9c6e8ac851638a9762ca5e120353bc8
MailineN <nguyen.mailine@gmail.com>
2022-04-14 14:00:06 +0200

    first commit

---
+++ src/main/java/com/example/protoolsactivitipoc/ProtoolsActivitiPocApplication.java
@@ -0,0 +1,13 @@
+package com.example.protoolsactivitipoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtoolsActivitiPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtoolsActivitiPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/protoolsactivitipoc/ProtoolsActivitiPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.protoolsactivitipoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtoolsActivitiPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
