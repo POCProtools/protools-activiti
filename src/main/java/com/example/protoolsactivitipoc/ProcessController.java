5e6034c4312cd718eb35566d040b681edd56ee28
MailineN <nguyen.mailine@gmail.com>
2022-04-15 16:32:17 +0200

    change info get tasks

--- src/main/java/com/example/protoolsactivitipoc/ProcessController.java
+++ src/main/java/com/example/protoolsactivitipoc/ProcessController.java
@@ -2,11 +2,13 @@ package com.example.protoolsactivitipoc;
 
 import com.example.protoolsactivitipoc.services.TaskRepresentation;
 import com.example.protoolsactivitipoc.util.Utils;
-import org.activiti.api.process.model.ProcessInstance;
 import org.activiti.api.process.model.builders.ProcessPayloadBuilder;
 import org.activiti.api.process.runtime.ProcessRuntime;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
+import org.activiti.engine.impl.persistence.entity.ExecutionEntity;
+import org.activiti.engine.impl.persistence.entity.TaskEntity;
+import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -32,30 +34,51 @@ public class ProcessController {
     @Autowired
     private TaskService taskService;
 
+    @GetMapping(value = "/start-categorize-process/")
+    public String startProcess(){
+        logger.info("> GET request to start the process: categorizeProcess");
+        String content = Utils.pickRandomString();
+
+        Map<String,Object> variables = new HashMap<String, Object>();
+        variables.put("content",content);
+
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yy HH:mm:ss");
+
+        logger.info("> Processing content: " + content + " at " + formatter.format(new Date()));
+
+        runtimeService.startProcessInstanceByKey("categorizeProcess", variables);
+
+        return(">>> Created Process Instance: "+ "categorizeProcess");
+    }
+
+
     @GetMapping(value = "/start-process/{processKey}")
     public String startProcess(@PathVariable String processKey){
         logger.info("> GET request to start the process: "+ processKey);
 
         runtimeService.startProcessInstanceByKey(processKey);
-
-        return(">>> Created Process Instance: "+ processKey);
-    }
-
-    @GetMapping("/get-tasks/{processID}")
-    public List<TaskRepresentation> getTasks(@PathVariable String processID){
-
-        List<Task> usertasks = taskService.createTaskQuery()
-                .processInstanceId(processID)
+        List<ProcessInstance> liste = runtimeService.createProcessInstanceQuery()
+                .processDefinitionKey(processKey)
                 .list();
 
-        return usertasks.stream()
-                .map(task -> new TaskRepresentation(task.getId(), task.getName(), task.getProcessInstanceId()))
-                .collect(Collectors.toList());
+        return(">>> Created Process Instance: "+ processKey+ " -- info: " + liste.toString());
+    }
+
+    @GetMapping("/get-tasks")
+    public void getTasks(){
+        List<ProcessInstance> data = runtimeService.createProcessInstanceQuery().active().list();
+        for(ProcessInstance processInstance : data) {
+            ExecutionEntity entity = (ExecutionEntity)processInstance;
+            List<Task> usertasks = taskService.createTaskQuery().processInstanceId(entity.getId()).list();
+            logger.info("Current task with ID : "+ usertasks.toString());
+        }
+
 
     }
 
     @GetMapping("/complete-task/{processID}")
     public String completeTask(@PathVariable String processID){
+
         Task task = taskService.createTaskQuery()
                 .processInstanceId(processID)
                 .singleResult();
